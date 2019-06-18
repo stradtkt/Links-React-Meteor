0775ca908b87e54c3edab16a94ae5892a9dc6878
Kevin <stradtkt22@gmail.com>
2019-06-18 16:29:45 -0400

    Created the login page

--- link/imports/ui/Login.js
+++ link/imports/ui/Login.js
@@ -1,10 +1,37 @@
 import React, {Component} from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Link } from 'react-router';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: ''
+        };
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        let email = this.refs.email.value.trim();
+        let password = this.refs.password.value.trim();
+        Meteor.loginWithPassword({email}, password, (err) => {
+            if(err) {
+                this.setState({error: 'Unable to log in.  Check email and password'});
+            } else {
+                this.setState({error: ''});
+            }
+        });
+    }
     render() {
         return (
             <div>
-                <p>Login Here</p>
+                <h1>Short Link</h1>
+                {this.state.error ? <p>{this.state.error}</p> : undefined}
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <input type="email" ref="email" name="email" placeholder="Email"/>
+                    <input type="password" ref="password" name="password" placeholder="Password"/>
+                    <button>Login</button>
+                </form>
+                <Link to="/signup">Have an account?</Link>
             </div>
         );
     }
