e452ad8144b0fe8624208d4a05b8f6137313ae54
Kevin <stradtkt22@gmail.com>
2019-06-18 16:54:01 -0400

    Created the schema

---
+++ link/imports/api/users.js
@@ -0,0 +1,18 @@
+import {Meteor} from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema';
+import {Accounts} from 'meteor/accounts-base';
+
+Accounts.validateNewUser((user) => {
+    const email = user.emails[0].address;
+    try {
+        new SimpleSchema({
+            email: {
+                type: String,
+                regEx: SimpleSchema.RegEx.Email
+            }
+        }).validate({email});
+    } catch (err) {
+        throw new Meteor.Error(400, err.message);
+    }
+    return true;
+});
